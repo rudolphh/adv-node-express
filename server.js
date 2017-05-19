106061fada511d820e856bb3fd350144a0c48e7d
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-05-18 19:36:42 -0700

    Set up a Template Engine

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
+const dotenv = require('dotenv').load();
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -11,9 +11,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render(process.cwd() + '/views/pug/index.pug');
   });
 
 app.listen(process.env.PORT || 3000, () => {
