12c5512975547c7b791b8d9418097f910e6b8c52
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-05-19 22:06:46 -0700

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -6,7 +6,32 @@ const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const dotenv = require('dotenv').load();
 const session = require('express-session');
 const passport = require('passport');
+
 const ObjectID = require('mongodb').ObjectID;
+const mongo = require('mongodb').MongoClient;
+
+mongo.connect(process.env.DATABASE, (err, db) => {
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+
+    //serialization and app.listen
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    passport.deserializeUser((id, done) => { 
+      db.collection('users').findOne({ _id: new ObjectID(id) }, (err, doc) => {
+        done(null, doc);
+      });
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
+});
 
 const app = express();
 
@@ -30,21 +55,3 @@ app.route('/')
   .get((req, res) => {
     res.render(process.cwd() + '/views/pug/index.pug', { title: 'Hello', message: 'Please login' });
   });
-
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => { /*
-  db.collection('users').findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, doc);
-  }); */
-  done(null, null);
-});
-
-
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
